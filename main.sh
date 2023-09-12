c921205a46f6695f40f1b6b5c0ebf40df102985d
Fabian Zierler <fabian.zierler@a1.net>
2023-09-12 15:36:13 +0200

    add activation and instantiation of julia deps

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 filename="out_scattering_I2"
 loglist="./input/isospin_logfiles_list"
 
+# activate correct julia environment and set up dependencies
+julia -e 'using Pkg; Pkg.activate("./scripts/src_jl"); Pkg.instantiate()'
+
 find ./input/  -name $filename  > $loglist
 python3 scripts/HDF5.py $loglist
 julia scripts/ensemble_table.jl

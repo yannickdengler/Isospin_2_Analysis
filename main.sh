7184de900917440770b46c9a58685b4f95c79852
Fabian Zierler <fabian.zierler@a1.net>
2024-03-13 17:29:14 +0000

    add fpi and plaquette to table

--- main.sh
+++ main.sh
@@ -3,14 +3,14 @@ loglist="./output/isospin_logfiles_list"
 
 # activate correct julia environment and set up dependencies
 julia -e 'using Pkg; Pkg.activate("./scripts/src_jl")'
-julia -e 'using Pkg; Pkg.activate("./scripts/src_jl"); Pkg.instantiate()'
+#julia -e 'using Pkg; Pkg.activate("./scripts/src_jl"); Pkg.instantiate()'
 
-find ./input/  -name "out_scattering_I2" > $loglistI2
-find ./input/  -name "out_spectrum" > $loglist
+#find ./input/  -name "out_scattering_I2" > $loglistI2
+#find ./input/  -name "out_spectrum" > $loglist
 #python3 scripts/HDF5.py $loglistI2
 
-julia scripts/ensemble_table.jl
-julia scripts/average.jl
-julia scripts/write_fpi_correlator.jl
-python3 scripts/fitting.py
+#julia scripts/ensemble_table.jl
+#julia scripts/average.jl
+#julia scripts/write_fpi_correlator.jl
+#python3 scripts/fitting.py
 julia scripts/energy_levels_table.jl
--- scripts/energy_levels_table.jl
+++ scripts/energy_levels_table.jl
@@ -2,6 +2,7 @@ using Pkg; Pkg.activate("./scripts/src_jl")
 using I2julia
 using HDF5
 using LaTeXStrings
+using Statistics
 
 function write_table_1(h5file;tabledir="output/tables")
     ispath(tabledir) || mkpath(tabledir)
@@ -12,16 +13,14 @@ function write_table_1(h5file;tabledir="output/tables")
     fid0  = h5open(h5file,"r")
     ensembles = keys(fid0)
 
-    io1 = open("$tabledir/overview_table.csv", "w");
-    io2 = open("$tabledir/overview_table_machine_readable.csv", "w");
-    io3 = open("$tabledir/energy_levels.csv", "w");
+    io1 = open("$tabledir/results.csv", "w");
+    io2 = open("$tabledir/results_readable.csv", "w");
     io4 = open("$tabledir/energy_levels_tex.csv", "w");
     #io = Base.stdout
 
-    println(io1,"group, beta, mass, L, T, N_conf, m_pi/m_rho, E_pipi")
-    println(io2,"group, beta, mass, L, T, N_conf, m_pi/m_rho, Delta_m_pi/m_rho, m_pi, Delta_m_pi, E_pipi, Delta_E_pipi")
-    println(io3,"group, beta, mass, L, T, N_conf, m_pi/m_rho, Delta_m_pi/m_rho, m_pi, Delta_m_pi, E_pipi, Delta_E_pipi")
-    println(io4,L"$\beta$ & $a m_{0}$ & $N_L$ & $N_T$ & $n_{\rm config}$ & $ m_\pi/m_\rho$ & $m_\pi$ & $E_{\pi\pi}$ \\\\")
+    println(io1,"group, beta, mass, L, T, N_conf, m_pi/m_rho, E_pipi, fpi, plaq")
+    println(io2,"group, beta, mass, L, T, N_conf, m_pi/m_rho, Delta_m_pi/m_rho, m_pi, Delta_m_pi, E_pipi, Delta_E_pipi, fpi, Delta_fpi, plaq, Delta_plaq")
+    println(io4,L"$\beta$ & $a m_{0}$ & $N_L$ & $N_T$ & $n_{\rm config}$ & $ m_\pi/m_\rho$ & $m_\pi$ & $E_{\pi\pi}$ & $f_{\pi}$ & $\langle P \rangle$ \\\\ \hline \hline")
 
     for ensemble in ensembles
 
@@ -42,31 +41,30 @@ function write_table_1(h5file;tabledir="output/tables")
         ΔEπ = fid["pi/Delta_E"][1]
         ΔEρ = fid["rho/Delta_E"][1]
 
+        # plaquette and pion decay constant
+        fπ  = fid["pi/fpi_ren"][]
+        Δfπ = fid["pi/Delta_fpi_ren"][]
+        plaq= fid["pi/plaquette"][]
+        p, Δp = mean(plaq), std(plaq)/sqrt(length(plaq))
+
         # chiral limit parameter
         ratioχ  = Eπ/Eρ 
         Δratioχ = Δratio(Eπ,Eρ,ΔEπ,ΔEρ)
-
-        # relative error
-        ΔrelEππ = ΔEππ/Eππ
-        ΔrelEρ  = ΔEρ/Eρ
-        ΔrelEρ  = ΔEρ/Eρ
-
-        # quality parameters
         χ2dof = fid["pipi/chi2"][] / fid["pipi/dof"][]
 
         str_rχ = errorstring(ratioχ, Δratioχ)
         str_ππ = errorstring(Eππ,ΔEππ)
         str_π  = errorstring(Eπ,ΔEπ)
+        str_fπ = errorstring(fπ,Δfπ)
+        str_p  = errorstring(p,Δp)
 
         # print to files
-        println(io1,"$group, $beta, $mass, $L, $T, $N, $str_rχ, $str_ππ")
-        println(io2,"$group, $beta, $mass, $L, $T, $N, $ratioχ, $Δratioχ, $Eπ, $ΔEπ, $Eππ, $ΔEππ")  
-        println(io3,"$group, $beta, $mass, $L, $T, $N, $ratioχ, $Δratioχ, $Eπ, $ΔEπ, $Eππ, $ΔEππ")
-        println(io4,"$beta & $mass & $L & $T & $N & $str_rχ & $str_π & $str_ππ \\\\")
+        println(io1,"$group, $beta, $mass, $L, $T, $N, $str_rχ, $str_ππ, $str_fπ, $str_p")
+        println(io2,"$group, $beta, $mass, $L, $T, $N, $ratioχ, $Δratioχ, $Eπ, $ΔEπ, $Eππ, $ΔEππ, $fπ, $Δfπ, $p, $Δp")  
+        println(io4,"$beta & $mass & $L & $T & $N & $str_rχ & $str_π & $str_ππ  & $str_fπ & $str_p \\\\")
     end
     close(io1)
     close(io2)
-    close(io3)
     close(io4)
     close(fid0)
 end
